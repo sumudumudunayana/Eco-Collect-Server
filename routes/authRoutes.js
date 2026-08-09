fddb21f0434607041ab86a83d2e82c4df5bf865b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-09 16:04:51 +0530

    routes updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,9 +6,7 @@ import {
 } from "../controllers/authController.js";
 
 const router = express.Router();
-
 router.post("/register", register);
-
 router.post("/login", login);
 
 export default router;
