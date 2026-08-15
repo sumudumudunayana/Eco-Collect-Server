9ec399dce44ff6bd95bf9efe2e09b8552ae238d7
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-15 21:24:39 +0530

    server updated

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+import "./config/env.js"; 
+import express from "express";import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import truckRoutes from "./routes/truckRoutes.js";
+import routeRoutes from "./routes/routeRoutes.js";
+import reportRoutes from "./routes/reportRoutes.js";
+import collectorRoutes from "./routes/collectorRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
-dotenv.config();
 
 connectDB();
 
@@ -21,7 +25,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
@@ -40,7 +44,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/collections", collectionRoutes);
 app.use("/api/products", productRoutes);
-
+app.use("/api/trucks", truckRoutes);
+app.use("/api/routes", routeRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/collector", collectorRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/upload", uploadRoutes);
 
 /* Test Route */
 
