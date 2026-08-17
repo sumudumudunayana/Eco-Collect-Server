5f1822529713988b10afe223175f78be183740e6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-17 19:08:50 +0530

    notification js updated

---
+++ models/Notification.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "Collection",
+        "Report",
+        "Marketplace",
+        "System",
+      ],
+      default: "System",
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification = mongoose.model(
+  "Notification",
+  notificationSchema
+);
+
+export default Notification;
