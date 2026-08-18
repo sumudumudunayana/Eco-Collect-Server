707adc7fb8326e4940025f5e1a8721b15faa07a4
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-18 22:20:22 +0530

    auth middleware updated

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Access denied. No token provided.",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select("-password");
+
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    next();
+    console.log("Authenticated User:", req.user.email);
+    console.log("Role:", req.user.role);
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Invalid token.",
+    });
+  }
+};
+
+export default protect;
