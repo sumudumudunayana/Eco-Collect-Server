0135b543d6ad710a83c3081d7e135eac0cc3d55b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-18 22:20:37 +0530

    role middleware updated

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to access this resource.",
+      });
+    }
+
+    next();
+  };
+};
+
+export default authorizeRoles;
